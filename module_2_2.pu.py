f9f260075033f9e4e62be44828e19f242af935f2
Vladimir1mir <korneevdeath@yandex.ru>
2024-09-03 21:21:01 +0300

    Add files via upload

---
+++ module_2_2.pu.py
@@ -0,0 +1,16 @@
+# first, second, third # first, second, third
+
+first = int(input("Введите первое число: "))
+second = int(input("Введите второе число: "))
+third = int(input("Введите третье число: "))
+
+if first == second == third:
+    print('Вывод: 3')
+
+elif (first == second or
+      first == third or
+      second == third):
+    print('Вывод: 2')
+
+else:
+    print('Вывод: 0')
